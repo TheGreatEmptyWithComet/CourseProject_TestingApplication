279fc2fde0e9cb442637bf48b583c14f16827479
DonKihot <donkihot1984@gmail.com>
2023-10-22 22:16:48 +0300

    Added default admin user

---
+++ TestingServerApp/Migrations/20231022175502_Create admin default user.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Collections;
+using System;
+using TestingServerApp.Utilites;
+
+#nullable disable
+
+namespace TestingServerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Createadmindefaultuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Create administrator group
+            migrationBuilder.Sql("INSERT INTO UserGroups (Name) VALUES ('Administrator')");
+
+            // Create default administrator user with salt and hash that correspond to "admin" password
+            byte[] passwordSaltArray = PasswordEncryption.GenerateSaltAsBytes();
+            string passwordSaltHexString = Convert.ToHexString(passwordSaltArray);
+            string passwordHash = PasswordEncryption.GetPasswordHash("admin", passwordSaltArray);
+
+            migrationBuilder.Sql($"INSERT INTO Users " +
+                $"(Login, Email, PasswordSalt, PasswordHash, UserGroupId) " +
+                $"VALUES " +
+                $"('admin','emailSample@emptyMail.com',X'{passwordSaltHexString}','{passwordHash}',1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Users");
+            migrationBuilder.Sql("DELETE FROM UserGroups");
+            // Reser identity column
+            migrationBuilder.Sql("DELETE FROM sqlite_sequence WHERE name='Users'");
+            migrationBuilder.Sql("DELETE FROM sqlite_sequence WHERE name='UserGroups'");
+        }
+    }
+}
---
+++ TestingServerApp/Utilites/PasswordEncryption.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingServerApp.Utilites
+{
+    public static class PasswordEncryption
+    {
+        public static byte[] GenerateSaltAsBytes(byte saltSize = 16)
+        {
+            byte[] salt = new byte[saltSize]; // 16 bytes is a common salt length
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+
+        public static string GetPasswordHash(string password, byte[] saltAsBytes)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] saltedPassword = new byte[saltAsBytes.Length + Encoding.UTF8.GetByteCount(password)];
+                Array.Copy(saltAsBytes, saltedPassword, saltAsBytes.Length);
+                Encoding.UTF8.GetBytes(password, 0, password.Length, saltedPassword, saltAsBytes.Length);
+
+                byte[] hashBytes = sha256.ComputeHash(saltedPassword);
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
